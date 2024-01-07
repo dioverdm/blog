2de6bc008d89e03a7b2254ab284db356951aee01
Dominik K <mezo.dsn@gmail.com>
2024-01-07 09:56:32 +0100

    fix: unchristmas fy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <meta
               key="og:image"
               property="og:image"
-              content="https://i.imgur.com/mtci9pz.png"
+              content="https://i.imgur.com/OPQaiVa.png"
             />
           </>
         )}
