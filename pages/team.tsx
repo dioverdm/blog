8ee69ed434803d8808fbff1fed6a36867bf43916
Dominik K <mezo.dsn@gmail.com>
2024-02-29 19:19:01 +0100

    fix: team page upgrade

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,4 +1,3 @@
-import commands from "../data/commands.json";
 import Head from "next/head";
 
 import Image from "next/image"; // Import the 'Image' component
@@ -6,44 +5,41 @@ import Image from "next/image"; // Import the 'Image' component
 export default function Team() {
   const staffs = [
     {
-      name: "Leonard Krasner",
-      id: 1,
-      role: "Senior Designer",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+      name: "Dominik Koch",
+      categorys: ["Developers"],
+      id: 2,
+      description: "Founder, Lead Developer, German Translator",
+      imageUrl: "/staff/Dominik.webp",
       xUrl: "#",
       linkedinUrl: "#",
     },
     {
-      name: "Leonard Krasner",
+      name: "Sky Skyer",
       id: 2,
-      role: "Senior Designer",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-      xUrl: "#",
-      linkedinUrl: "#",
-    },
-    {
-      name: "Leonard Krasner",
-      id: 2,
-      role: "Senior Designer",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-      xUrl: "#",
-      linkedinUrl: "#",
-    },
-    {
-      name: "Leonard Krasner",
-      id: 2,
-      role: "Senior Designer",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+      categorys: [
+        "Developers",
+        "Designers",
+        "Beta Testers",
+        "Translators",
+        "Contributors",
+      ],
+      description: "CO-Founder, Developer",
+      imageUrl: "/staff/Sky.webp",
       xUrl: "#",
       linkedinUrl: "#",
     },
     //
     // More people...
   ];
+
+  const roles = [
+    "Developers",
+    "Designers",
+    "Beta Testers",
+    "Translators",
+    "Contributors",
+  ];
+
   return (
     <>
       <Head>
@@ -61,69 +57,73 @@ export default function Team() {
         <p className="text-neutral-500">
           Meet the amazing team and contributors behind Would You.
         </p>
-        <div className="mx-auto max-w-5xl text-center">
-          <ul
-            role="list"
-            className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4 lg:gap-8"
-          >
-            {staffs.map((staff) => (
-              // eslint-disable-next-line react/jsx-key
-              <li
-                key={staff.id} // Add a unique key prop
-                className="rounded-2xl bg-neutral-800 py-8"
+        <div className="mx-auto h-full max-w-5xl justify-center text-center">
+          {roles.map((role) => (
+            <>
+              <h2 className=" mt-10 select-none font-semibold text-neutral-300">
+                {role}
+              </h2>
+              <ul
+                role="list"
+                className="text-pretty mx-auto mt-10 grid max-w-2xl grid-cols-1 justify-center gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4 lg:gap-8 "
               >
-                <img
-                  className="mx-auto h-32 w-32 rounded-full md:h-32 md:w-32"
-                  src={staff.imageUrl}
-                  width={32}
-                  height={32}
-                  alt=""
-                />
-                <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">
-                  {staff.name}
-                </h3>
-                <p className="text-sm leading-6 text-gray-400">{staff.role}</p>
-                <ul role="list" className="mt-6 flex justify-center gap-x-6">
-                  <li>
-                    <a
-                      href={staff.xUrl} // Use the correct variable name
-                      className="text-gray-400 hover:text-gray-300"
+                {staffs
+                  .filter((staff) => staff.categorys.includes(role))
+                  .map((staff) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <li
+                      key={staff.id} // Add a unique key prop
+                      className="transform content-center rounded-2xl bg-neutral-800 px-4 py-8 shadow-lg transition duration-300 ease-in-out hover:shadow-2xl sm:px-6 lg:px-8 lg:hover:-translate-y-2 xl:px-10"
                     >
-                      <span className="sr-only">X</span>
-                      <svg
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
+                      <Image
+                        className="mx-auto h-28 w-28 rounded-full md:h-24 md:w-24"
+                        src={staff.imageUrl}
+                        width={28}
+                        height={28}
+                        alt=""
+                      />
+                      <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">
+                        {staff.name}
+                      </h3>
+                      <p className="text-sm leading-6 text-gray-400">
+                        {staff.description}
+                      </p>
+                      <ul
+                        role="list"
+                        className="mt-6 flex justify-center align-bottom"
                       >
-                        <path d="M11.4678 8.77491L17.2961 2H15.915L10.8543 7.88256L6.81232 2H2.15039L8.26263 10.8955L2.15039 18H3.53159L8.87581 11.7878L13.1444 18H17.8063L11.4675 8.77491H11.4678ZM9.57608 10.9738L8.95678 10.0881L4.02925 3.03974H6.15068L10.1273 8.72795L10.7466 9.61374L15.9156 17.0075H13.7942L9.57608 10.9742V10.9738Z" />
-                      </svg>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href={staff.linkedinUrl} // Use the correct variable name
-                      className="text-gray-400 hover:text-gray-300"
-                    >
-                      <span className="sr-only">LinkedIn</span>
-                      <svg
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fillRule="evenodd" // Use the correct attribute name
-                          d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                          clipRule="evenodd" // Use the correct attribute name
-                        />
-                      </svg>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            ))}{" "}
-          </ul>
+                        <li
+                          className="absolute bottom-5 left-1/2 -translate-x-1/2"
+                          key={staff.linkedinUrl}
+                        >
+                          <a
+                            href={staff.linkedinUrl} // Use the correct variable name
+                            className="text-gray-400 hover:text-gray-300"
+                          >
+                            <span className="sr-only">LinkedIn</span>
+                            <svg
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                              fill="currentColor"
+                              viewBox="0 0 120 120"
+                            >
+                              <switch>
+                                <g>
+                                  <path d="M60 120C26.9 120 0 93.1 0 60S26.9 0 60 0s60 26.9 60 60-26.9 60-60 60M60 5C29.7 5 5 29.7 5 60s24.7 55 55 55 55-24.7 55-55S90.3 5 60 5" />
+                                  <path d="M60 120c-19.3 0-34.4-26.4-34.4-60S40.7 0 60 0s34.4 26.4 34.4 60-15.1 60-34.4 60M60 5C43.8 5 30.5 29.7 30.5 60s13.2 55 29.5 55 29.5-24.7 29.5-55S76.2 5 60 5" />
+                                  <path d="M12.2 25.6h95.6v5H12.2zm0 63.9h95.6v5H12.2zm-9.7-32h115v5H2.5z" />
+                                  <path d="M57.5 2.5h5v115h-5z" />
+                                </g>
+                              </switch>
+                            </svg>
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  ))}{" "}
+              </ul>
+            </>
+          ))}{" "}
         </div>
       </main>
     </>
