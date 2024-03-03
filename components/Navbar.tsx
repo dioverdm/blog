8f878a7f857fd0b077162f74b6bf67a12e805415
mezotv <mezotv@users.noreply.github.com>
2024-03-03 01:28:07 +0000

    chore(style): format files

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -63,118 +63,118 @@ const Navbar = () => {
   };
 
   return (
-       <nav className="fixed left-0 top-0 z-50 mb-28 flex h-[80px] w-full items-center justify-between border-b border-b-neutral-800 bg-neutral-900 bg-opacity-90 backdrop-blur-sm">
-        <div className="ml-8 flex items-center xl:ml-[17vw]">
-          <Link href="/">
-            <div className="flex items-center">
-              <Image
-                src="/Logo.svg"
-                className="rounded-full"
-                alt="Would You Logo"
-                width="50"
-                height="50"
-                priority
-              />
-              <p className="ml-4 text-2xl font-bold text-white">Would You</p>
-            </div>
+    <nav className="fixed left-0 top-0 z-50 mb-28 flex h-[80px] w-full items-center justify-between border-b border-b-neutral-800 bg-neutral-900 bg-opacity-90 backdrop-blur-sm">
+      <div className="ml-8 flex items-center xl:ml-[17vw]">
+        <Link href="/">
+          <div className="flex items-center">
+            <Image
+              src="/Logo.svg"
+              className="rounded-full"
+              alt="Would You Logo"
+              width="50"
+              height="50"
+              priority
+            />
+            <p className="ml-4 text-2xl font-bold text-white">Would You</p>
+          </div>
+        </Link>
+        <div className="ml-24 hidden items-center md:flex">
+          <Link
+            href="/commands"
+            className="mr-6 text-lg text-neutral-300 transition-all hover:text-neutral-100"
+          >
+            Commands
           </Link>
-          <div className="ml-24 hidden items-center md:flex">
+          <Link
+            href="/blog"
+            className="mr-6 text-lg text-neutral-300 transition-all hover:text-neutral-100"
+          >
+            Blog
+          </Link>
+          <Link
+            href="/vote"
+            target="_blank"
+            className="mr-6 text-lg text-neutral-300 transition-all hover:text-neutral-100"
+          >
+            Vote
+          </Link>
+        </div>
+      </div>
+      <div className="z-50 mr-8 flex items-center xl:mr-[17vw]">
+        <Link href="/invite" target="_blank" className="hidden md:block">
+          <Button className="">Invite</Button>
+        </Link>
+        <div
+          className="relative ml-6 flex h-6 w-8 flex-col items-center justify-between md:hidden"
+          onClick={() => toggleMobileMenu()}
+        >
+          <LazyMotion features={domAnimation}>
+            <m.span
+              className="h-[3px] w-[30px] origin-left rounded-full bg-neutral-300"
+              initial={{ rotate: "0deg" }}
+              animate={lineOneControls}
+            />
+            <m.span
+              className="menu-line line-1 h-[3px] w-[30px] rounded-full bg-neutral-300"
+              initial={{ opacity: 1 }}
+              animate={lineTwoControls}
+            />
+            <m.span
+              className="menu-line line-1 h-[3px] w-[30px] origin-left rounded-full bg-neutral-300"
+              initial={{ rotate: "0deg" }}
+              animate={lineThreeControls}
+            />
+          </LazyMotion>
+        </div>
+      </div>
+      <LazyMotion features={domAnimation}>
+        <m.div
+          className="fixed left-0 top-0 z-40 h-[100vh] w-[100vw] bg-neutral-900"
+          transition={{ duration: 0.21, type: "easeInOut" }}
+          initial={{ opacity: 0.5, left: "100vw", pointerEvents: "none" }}
+          animate={menuControls}
+        >
+          <div className="absolute top-36 flex w-full flex-col items-center">
+            <Link
+              href="/"
+              className="mt-8 text-center text-3xl text-white"
+              onClick={() => toggleMobileMenu()}
+            >
+              Home
+            </Link>
             <Link
               href="/commands"
-              className="mr-6 text-lg text-neutral-300 transition-all hover:text-neutral-100"
+              className="mt-8 text-center text-3xl text-white"
+              onClick={() => toggleMobileMenu()}
             >
               Commands
             </Link>
             <Link
               href="/blog"
-              className="mr-6 text-lg text-neutral-300 transition-all hover:text-neutral-100"
+              className="mt-8 text-center text-3xl text-white"
+              onClick={() => toggleMobileMenu()}
             >
               Blog
             </Link>
             <Link
               href="/vote"
               target="_blank"
-              className="mr-6 text-lg text-neutral-300 transition-all hover:text-neutral-100"
+              className="mt-8 text-center text-3xl text-white"
+              onClick={() => toggleMobileMenu()}
             >
               Vote
             </Link>
+            <Link
+              href="/invite"
+              target="_blank"
+              className="mt-8 text-center text-2xl"
+            >
+              <Button>Invite</Button>
+            </Link>
           </div>
-        </div>
-        <div className="z-50 mr-8 flex items-center xl:mr-[17vw]">
-          <Link href="/invite" target="_blank" className="hidden md:block">
-            <Button className="">Invite</Button>
-          </Link>
-          <div
-            className="relative ml-6 flex h-6 w-8 flex-col items-center justify-between md:hidden"
-            onClick={() => toggleMobileMenu()}
-          >
-            <LazyMotion features={domAnimation}>
-              <m.span
-                className="h-[3px] w-[30px] origin-left rounded-full bg-neutral-300"
-                initial={{ rotate: "0deg" }}
-                animate={lineOneControls}
-              />
-              <m.span
-                className="menu-line line-1 h-[3px] w-[30px] rounded-full bg-neutral-300"
-                initial={{ opacity: 1 }}
-                animate={lineTwoControls}
-              />
-              <m.span
-                className="menu-line line-1 h-[3px] w-[30px] origin-left rounded-full bg-neutral-300"
-                initial={{ rotate: "0deg" }}
-                animate={lineThreeControls}
-              />
-            </LazyMotion>
-          </div>
-        </div>
-        <LazyMotion features={domAnimation}>
-          <m.div
-            className="fixed left-0 top-0 z-40 h-[100vh] w-[100vw] bg-neutral-900"
-            transition={{ duration: 0.21, type: "easeInOut" }}
-            initial={{ opacity: 0.5, left: "100vw", pointerEvents: "none" }}
-            animate={menuControls}
-          >
-            <div className="absolute top-36 flex w-full flex-col items-center">
-              <Link
-                href="/"
-                className="mt-8 text-center text-3xl text-white"
-                onClick={() => toggleMobileMenu()}
-              >
-                Home
-              </Link>
-              <Link
-                href="/commands"
-                className="mt-8 text-center text-3xl text-white"
-                onClick={() => toggleMobileMenu()}
-              >
-                Commands
-              </Link>
-              <Link
-                href="/blog"
-                className="mt-8 text-center text-3xl text-white"
-                onClick={() => toggleMobileMenu()}
-              >
-                Blog
-              </Link>
-              <Link
-                href="/vote"
-                target="_blank"
-                className="mt-8 text-center text-3xl text-white"
-                onClick={() => toggleMobileMenu()}
-              >
-                Vote
-              </Link>
-              <Link
-                href="/invite"
-                target="_blank"
-                className="mt-8 text-center text-2xl"
-              >
-                <Button>Invite</Button>
-              </Link>
-            </div>
-          </m.div>
-        </LazyMotion>
-      </nav>
+        </m.div>
+      </LazyMotion>
+    </nav>
   );
 };
 
--- pages/commands.tsx
+++ pages/commands.tsx
@@ -7,11 +7,7 @@ export default function Commands() {
   const [isHovered, setIsHovered] = useState(false);
   const [copyText, setCopyText] = useState("");
 
-  const categories = [
-    "Settings",
-    "Utility",
-    "Games",
-  ];
+  const categories = ["Settings", "Utility", "Games"];
 
   const handleCopyUsage = (textToCopy: any) => {
     const commandPart = textToCopy.split(" ")[0];
@@ -35,106 +31,107 @@ export default function Commands() {
         </h1>
 
         <div className="mt-8 flex flex-col gap-4">
-        {categories.map((category) => (
+          {categories.map((category) => (
             <>
               <h2 className=" mt-10 select-none font-semibold text-neutral-300">
                 {category}
               </h2>
-          {commands.filter((command) => command.category.includes(category)).map((command) => {
-            const isActive = openedCommand === command.name;
+              {commands
+                .filter((command) => command.category.includes(category))
+                .map((command) => {
+                  const isActive = openedCommand === command.name;
 
-            return (
-              <div
-                className={`relative cursor-pointer overflow-hidden rounded-lg p-4 text-neutral-300 transition-all duration-300 ${
-                  openedCommand === command.name
-                    ? "max-h-[250px] bg-neutral-700"
-                    : "max-h-[90px] bg-neutral-800"
-                }`}
-                onClick={() =>
-                  isActive
-                    ? setOpenedCommand("")
-                    : setOpenedCommand(command.name)
-                }
-                key={command.name}
-              >
-                <div className="flex items-center justify-between">
-                  <div className="grow overflow-hidden">
-                    <h4 className="mb-1 text-lg font-bold text-white">
-                      <span className="mr-0.5 text-neutral-500">/</span>
-                      {command.name}
-                    </h4>
-                    <p className="mb-3 max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-                      {command.description}
-                    </p>
-                  </div>
-                  <div className="h-[30px] w-[30px]">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="30"
-                      height="30"
-                      fill="currentColor"
-                      viewBox="0 0 30 30"
-                      className={`transition-all duration-300 ${
-                        isActive
-                          ? "rotate-180 text-neutral-300"
-                          : "text-neutral-500"
+                  return (
+                    <div
+                      className={`relative cursor-pointer overflow-hidden rounded-lg p-4 text-neutral-300 transition-all duration-300 ${
+                        openedCommand === command.name
+                          ? "max-h-[250px] bg-neutral-700"
+                          : "max-h-[90px] bg-neutral-800"
                       }`}
+                      onClick={() =>
+                        isActive
+                          ? setOpenedCommand("")
+                          : setOpenedCommand(command.name)
+                      }
+                      key={command.name}
                     >
-                      <path d="M15 20.938a.93.93 0 0 1-.663-.275l-8.75-8.75a.938.938 0 1 1 1.327-1.327L15 18.674l8.088-8.088a.938.938 0 1 1 1.326 1.327l-8.75 8.75a.94.94 0 0 1-.665.274Z" />
-                    </svg>
-                  </div>
-                </div>
-                <div
-                  className={`transition-all duration-300 ${
-                    isActive ? "opacity-100" : "opacity-0"
-                  }`}
-                >
-                  <h5 className="mb-1">Usage</h5>
-                  <h6
-                    className="mb-2 w-fit rounded-md bg-neutral-900 px-2 py-1 font-mono text-xs"
-                    onMouseEnter={() => {
-                      setCopyText("--Click To Copy--");
-                      setIsHovered(true);
-                    }}
-                    onMouseLeave={() => {
-                      setCopyText(command.usage);
-                      setIsHovered(false);
-                    }}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleCopyUsage(command.usage);
-                    }}
-                  >
-                    {isHovered ? (
-                      <div className="text-gray-400">{copyText}</div>
-                    ) : (
-                      <div>{command.usage}</div>
-                    )}
-                  </h6>
-                  {command.subcommands && (
-                    <>
-                      <h5 className="mb-1">Subcommands</h5>
-                      <h6 className="w-fit rounded-md bg-neutral-900 px-2 py-1 font-mono text-xs">
-                        {command.subcommands.join(", ")}
-                      </h6>
-                    </>
-                  )}
-                  {command.options && (
-                    <>
-                      <h5 className="mb-1">Options</h5>
-                      <h6 className="w-fit rounded-md bg-neutral-900 px-2 py-1 font-mono text-xs">
-                        {command.options.join(", ")}
-                      </h6>
-                    </>
-                  )}
-                </div>
-              </div>
-            );
-          })}
-        </>
-             ))}{" "}
+                      <div className="flex items-center justify-between">
+                        <div className="grow overflow-hidden">
+                          <h4 className="mb-1 text-lg font-bold text-white">
+                            <span className="mr-0.5 text-neutral-500">/</span>
+                            {command.name}
+                          </h4>
+                          <p className="mb-3 max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+                            {command.description}
+                          </p>
+                        </div>
+                        <div className="h-[30px] w-[30px]">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="30"
+                            height="30"
+                            fill="currentColor"
+                            viewBox="0 0 30 30"
+                            className={`transition-all duration-300 ${
+                              isActive
+                                ? "rotate-180 text-neutral-300"
+                                : "text-neutral-500"
+                            }`}
+                          >
+                            <path d="M15 20.938a.93.93 0 0 1-.663-.275l-8.75-8.75a.938.938 0 1 1 1.327-1.327L15 18.674l8.088-8.088a.938.938 0 1 1 1.326 1.327l-8.75 8.75a.94.94 0 0 1-.665.274Z" />
+                          </svg>
+                        </div>
+                      </div>
+                      <div
+                        className={`transition-all duration-300 ${
+                          isActive ? "opacity-100" : "opacity-0"
+                        }`}
+                      >
+                        <h5 className="mb-1">Usage</h5>
+                        <h6
+                          className="mb-2 w-fit rounded-md bg-neutral-900 px-2 py-1 font-mono text-xs"
+                          onMouseEnter={() => {
+                            setCopyText("--Click To Copy--");
+                            setIsHovered(true);
+                          }}
+                          onMouseLeave={() => {
+                            setCopyText(command.usage);
+                            setIsHovered(false);
+                          }}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleCopyUsage(command.usage);
+                          }}
+                        >
+                          {isHovered ? (
+                            <div className="text-gray-400">{copyText}</div>
+                          ) : (
+                            <div>{command.usage}</div>
+                          )}
+                        </h6>
+                        {command.subcommands && (
+                          <>
+                            <h5 className="mb-1">Subcommands</h5>
+                            <h6 className="w-fit rounded-md bg-neutral-900 px-2 py-1 font-mono text-xs">
+                              {command.subcommands.join(", ")}
+                            </h6>
+                          </>
+                        )}
+                        {command.options && (
+                          <>
+                            <h5 className="mb-1">Options</h5>
+                            <h6 className="w-fit rounded-md bg-neutral-900 px-2 py-1 font-mono text-xs">
+                              {command.options.join(", ")}
+                            </h6>
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  );
+                })}
+            </>
+          ))}{" "}
         </div>
-      
       </main>
     </>
   );
